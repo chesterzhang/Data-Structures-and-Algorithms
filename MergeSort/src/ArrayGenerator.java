0526184977378d556c081da13e9f875efecb5982
chesterzhang <chesterzhang666@gmail.com>
2021-08-02 21:56:40 +0800

    added MergeSort

---
+++ MergeSort/src/ArrayGenerator.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class ArrayGenerator {
+    private ArrayGenerator() {
+    }
+
+    public static   Integer[] generateOrderedArray(int n){
+        Integer[] arr= new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=i;
+        }
+        return arr;
+    }
+
+    /**
+     * 生成一个长度为n的随机数组arr, 每个数字的范围是[0,bound)
+     * @param n
+     * @param bound
+     * @return arr
+     */
+    public static   Integer[] generateRandomArray(int n, int bound){
+        Integer[] arr = new Integer[n];
+        Random rnd= new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i]=rnd.nextInt(bound);
+        }
+        return arr;
+    }
+}
